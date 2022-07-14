6ae524109d90bd018853c71cd3ac641a317d2f9c
Ujjawal <ujjawal.anand21@gmail.com>
2022-07-14 15:16:33 +0530

    Framework Added

---
+++ src/main/java/libraries/Base.java
@@ -0,0 +1,49 @@
+package libraries;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Base {
+
+	public static WebDriver driver;
+	public static Properties prop;
+	
+
+	public static Logger logger = LoggerFactory.getLogger(Base.class);
+	
+	public Base(){
+		FileInputStream fin;
+		try {
+			prop = new Properties();
+			fin = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/Other/Config.properties");
+			try {
+				prop.load(fin);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void init() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get(prop.getProperty("url"));
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		logger.info("Launched browser successfully");
+	}
+	
+	
+}
---
+++ src/main/java/libraries/ExtentReportListener.java
@@ -0,0 +1,137 @@
+package libraries;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class ExtentReportListener extends Base implements ITestListener{
+	private static final String OUTPUT_FOLDER = System.getProperty("user.dir") + "\\src\\test\\resources\\report\\";
+	private static final String FILE_NAME = "TestExecutionReport.html";
+
+	private static ExtentReports extent = initReady();
+	public static ThreadLocal<ExtentTest> test = new ThreadLocal<ExtentTest>();
+
+	private static ExtentReports initReady() {
+
+		Path path = Paths.get(OUTPUT_FOLDER);
+		// if directory exists?
+		if (!Files.exists(path)) {
+			try {
+				Files.createDirectories(path);
+			} catch (IOException e) {
+				// fail to create directory
+				e.printStackTrace();
+			}
+		}
+		ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(OUTPUT_FOLDER + FILE_NAME);
+		htmlReporter.config().setDocumentTitle("Automation Test Results");
+		htmlReporter.config().setReportName("Automation Test Results");
+		htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+		htmlReporter.config().setTheme(Theme.STANDARD);
+
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
+		extent.setReportUsesManualConfiguration(true);
+
+		return extent;
+	}
+
+	public synchronized void onStart(ITestContext context) {
+		System.out.println("Test Suite started!");
+	}
+
+	public synchronized void onFinish(ITestContext context) {
+		System.out.println(("Test Suite is ending!"));
+		extent.flush();
+		test.remove();
+	}
+
+	public synchronized void onTestStart(ITestResult result) {
+		String methodName = result.getMethod().getMethodName();
+		String qualifiedName = result.getMethod().getQualifiedName();
+		int last = qualifiedName.lastIndexOf(".");
+		int mid = qualifiedName.substring(0, last).lastIndexOf(".");
+		String className = qualifiedName.substring(mid + 1, last);
+
+		System.out.println(methodName + " started!");
+		ExtentTest extentTest = extent.createTest(result.getMethod().getMethodName(),
+				result.getMethod().getDescription());
+
+		extentTest.assignCategory(result.getTestContext().getSuite().getName());
+		/*
+		 * methodName = StringUtils.capitalize(StringUtils.join(StringUtils.
+		 * splitByCharacterTypeCamelCase(methodName), StringUtils.SPACE));
+		 */
+		extentTest.assignCategory(className);
+		test.set(extentTest);
+		test.get().getModel().setStartTime(getTime(result.getStartMillis()));
+	}
+
+	public synchronized void onTestSuccess(ITestResult result) {
+		System.out.println((result.getMethod().getMethodName() + " passed!"));
+		test.get().pass("Test passed");
+		test.get().getModel().setEndTime(getTime(result.getEndMillis()));
+	}
+
+	public synchronized void onTestFailure(ITestResult result) {
+		System.out.println((result.getMethod().getMethodName() + " failed!"));
+		try {
+			test.get().fail(result.getThrowable(),
+					MediaEntityBuilder.createScreenCaptureFromPath(getScreenshot(result.getMethod().getMethodName())).build());
+		} catch (IOException e) {
+			System.err
+					.println("Exception thrown while updating test fail status " + Arrays.toString(e.getStackTrace()));
+		}
+		test.get().getModel().setEndTime(getTime(result.getEndMillis()));
+	}
+
+	public synchronized void onTestSkipped(ITestResult result) {
+		System.out.println((result.getMethod().getMethodName() + " skipped!"));
+		try {
+			test.get().skip(result.getThrowable(),
+					MediaEntityBuilder.createScreenCaptureFromPath(getScreenshot(result.getMethod().getMethodName())).build());
+		} catch (IOException e) {
+			System.err
+					.println("Exception thrown while updating test skip status " + Arrays.toString(e.getStackTrace()));
+		}
+		test.get().getModel().setEndTime(getTime(result.getEndMillis()));
+	}
+
+	public synchronized void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		System.out.println(("onTestFailedButWithinSuccessPercentage for " + result.getMethod().getMethodName()));
+	}
+
+	private Date getTime(long millis) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(millis);
+		return calendar.getTime();
+	}
+	
+	public String getScreenshot(String testcaseName) throws IOException {
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String currentDir = System.getProperty("user.dir");
+		String destPath = currentDir +"/src/test/resources/screenshot/"+testcaseName+".png";
+		FileUtils.copyFile(scrFile, new File(destPath));
+		return destPath;
+	}
+
+}
